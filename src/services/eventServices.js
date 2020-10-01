9a0341a9345318941a068dd9371a46deb5b4087a
Jam Wheat <jamonwheat@gmail.com>
2020-10-01 17:43:32 -0400

    create eventServices.js

---
+++ src/services/eventServices.js
@@ -0,0 +1,11 @@
+import tokenService from './tokenService'
+const BASE_URL = "/api/events/";
+
+export function create(event) {
+  return fetch(BASE_URL, {
+      method: "POST",
+      headers: { 'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken() },
+      body: JSON.stringify(event)
+  }, { mode: "cors" })
+      .then(res => res.json());
+}
