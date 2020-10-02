7e3d5abfd385ecf440f000e9dce943177f918e21
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-10-01 20:56:08 -0400

    adding template table with function to grab and show calendar data

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,18 +4,34 @@ import './Profile.css'
 export default class userProfile extends Component{
    
    render(){
-      let usrId = this.props.history.location.pathname
-      usrId = usrId.split('/')
+      let usrId = this.props.history.location.pathname.split('/')
       usrId = usrId[2]
+      let x=[]
+      // this will be used to grab all the event data and format it into a table. for loop will be replaced with map
+      function eventDates(){
+         for(let i = 0; i < 10; i++)
+         {x.push(<tr><td>calender</td><td>data</td><td>goes</td><td>here</td></tr>)}
+         return x
+      }
+      
       return(
          <>
          { this.props.user!==null ?
             <div className="profile">
                <div className='L'>
-                  <h1>Hello{usrId}!</h1>
+                  <h1>Hello!</h1>
                </div>
                <div className='C'>
-                  <h1>Hello {this.props.user._id===usrId?'h':'nope'}!</h1>
+                  <h1>Hello {this.props.user._id===usrId?'yeah':'nope'}!</h1>
+                  <div>
+                     <table>
+                        <tbody>
+                           <tr><td colSpan='2'>name: {this.props.user.name}</td><td colSpan='2'>other info</td></tr>
+                           <tr><td colSpan='4'>bio</td></tr>
+                           {eventDates()}
+                        </tbody>
+                     </table>
+                  </div>
                </div>
                <div className='R'>
                   <h1>Hello!</h1>
@@ -28,6 +44,15 @@ export default class userProfile extends Component{
                </div>
                <div className='C'>
                   <h1>Hello stranger!</h1>
+                  <div>
+                     <table>
+                        <tbody>
+                           <tr><td colSpan='2'>name goes here</td><td colSpan='2'>other info</td></tr>
+                           <tr><td colSpan='4'>bio</td></tr>
+                           {eventDates()}
+                        </tbody>
+                     </table>
+                  </div>
                </div>
                <div className='R'>
                   <h1>Hello!</h1>
